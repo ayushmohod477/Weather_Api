2c7a49140ff57fa14d7bfb0bc7af4b2061d21bc9
Ayush Mohod <ayushmohod36@gmail.com>
2026-09-20 02:08:28 +0530

    add yearly and station wise data feature

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    station = pd.read_csv("data_small/stations.txt", skiprows=17)
+    station = station[["STAID", "STANAME                                 "]]
+    return render_template("home.html", data=station.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -15,6 +17,25 @@ def about(station, date):
     df = pd.read_csv(filename, skiprows=20)
     temperature = df.loc[df["    DATE"] == int(date)]["   TG"].squeeze() / 10
     return {"station": station, "date": date, "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station.zfill(6)) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station.zfill(6)) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+
 
 
 if __name__ == "__main__":
